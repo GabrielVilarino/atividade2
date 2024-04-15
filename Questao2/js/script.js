37cf0b79186d0f08e06541c228ae6d66a27ac498
Gabriel Vilarino <gabriel_vilarino@hotmail.com>
2024-04-15 16:32:56 -0300

    Alteração caminho das imagens

--- Questao2/js/script.js
+++ Questao2/js/script.js
@@ -2,12 +2,12 @@ function alterarImagem(){
     var btnImage = document.getElementById('360')
     var sofaImage = document.getElementById('sofa')
     //alert(btnImage.src)
-    if (btnImage.src == 'http://127.0.0.1:5500/Questao2/src/btn360.png') {
-        btnImage.src = 'http://127.0.0.1:5500/Questao2/src/fechar.png'
-        sofaImage.src = 'http://127.0.0.1:5500/Questao2/src/gif.gif'
+    if (btnImage.src == 'https://atividade2-rho.vercel.app/Questao2/src/btn360.png') {
+        btnImage.src = 'https://atividade2-rho.vercel.app/Questao2/src/fechar.png'
+        sofaImage.src = 'https://atividade2-rho.vercel.app/Questao2/src/gif.gif'
     }
     else{
-        btnImage.src = 'http://127.0.0.1:5500/Questao2/src/btn360.png'
-        sofaImage.src = 'http://127.0.0.1:5500/Questao2/src/image.png'
+        btnImage.src = 'https://atividade2-rho.vercel.app/Questao2/src/btn360.png'
+        sofaImage.src = 'https://atividade2-rho.vercel.app/Questao2/src/image.png'
     }
 }
